1c2c1555d6054919630a857edd1d551f237edffa
David Kraft <dk@dkraft.net>
2019-03-13 19:47:25 -0500

    Created skeleton for script.js -dk

---
+++ assets/javascript/script.js
@@ -0,0 +1,55 @@
+// ORGANIZATION OF SCRIPT:
+//   1.) Global Variables Obj
+//   2.) User Input Obj
+//   3.) APIs Obj
+//     a.) YouTube Process Obj
+//     b.) MusixMatch Process Obj
+//   4.) Output to User Obj
+
+// GLOBAL VARS OBJ
+glblVars = {
+    lyricsInp: "",
+    songsInp: "",
+    ytRes: "",
+    mmRes: ""
+}
+
+// USER INPUT OBJ
+userInp = {
+
+    //HANDLE LYRICS INPUT
+    storeLyricsInput: function() {
+
+    },
+    validateLyricsInput: function () {
+
+    },
+
+    //HANDLE SONG INPUT
+    storeSongInput: function () {
+
+    },
+    validateSongInput: function () {
+
+    },
+
+}
+
+// APIS OBJ
+api = {
+
+//    YOUTUBE OBJECT
+    yt: {
+
+    },
+
+//    MUSIXMATCH OBJECT
+    mm: {
+
+    }
+}
+
+//OUTPUT TO USER
+output = {
+
+}
